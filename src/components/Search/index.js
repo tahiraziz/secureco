d73ad7693cc6075aa2eb17d4bcba503ffd13995a
Tahir Aziz <tahir.aziz4@gmail.com>
2017-10-31 14:18:58 -0500

    made search responsoive

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -6,11 +6,13 @@ class Search extends Component {
     render() {
         return(
             <nav class="search navbar sticky-top">
-                <a class="navbar-brand">Enterprise Domain Name</a>
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search in Domain" aria-label="Search"/>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><img src={search} alt="search" width="16px" height="16px"/></button>
-                </form>
+                <div class="searchbar">
+                    <a class="navbar-brand">Enterprise Domain Name</a>
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search in Domain" aria-label="Search"/>
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><img src={search} alt="search" width="16px" height="16px"/></button>
+                    </form>
+                </div>
                 <div class="btn-group justify-content-end" role="group" aria-label="Basic example">
                     <button type="button" class="btn btn-secondary">Left</button>
                     <button type="button" class="btn btn-secondary">Middle</button>
